29a383386184ea89bda1ad6b0c2b1afc6cc9bc1e
DNAMcKnight <conner.cmk9040@gmail.com>
2022-12-13 02:44:37 +0600

    moved samba script to it's own file and added small checks lots more to do

--- setup.sh
+++ setup.sh
@@ -2,94 +2,51 @@
 
 
 screenSetup () {
-    echo "Installing screen..."
-    sudo apt-get install screen
-    echo "Screen installation complete!"
+    which screen > /dev/null
+    if [ $? -eq 0 ]; then
+        tput setaf 1; echo "Screen already installed!"
+    else
+        echo "Installing screen..."
+        sudo apt-get install screen
+        tput setaf 2; echo "Screen installation complete!"
+        exit 1
+    fi
 }
 
 
 microSetup () {
-    echo "Downloading micro..."
-    curl https://getmic.ro | bash
-    echo "Changing permissions for micro"
-    sudo chown root:root micro
-    echo "Micro installation complete!"
-    sudo mv micro /usr/local/bin
-    echo "Moving micro to /usr/local/bin"
-    which micro
-}
-
-
-smbConfWrite () {
-    echo "
-[usb]
-    comment = Media Share
-    path = /media/USBDrive
-    writeable = yes
-    force group = pi
-    create mask = 0660
-    directory mask = 0771
-    read only = no
-
-[pi-share]
-    comment = Pi Share
-    path = /home/pi
-    writeable = yes
-    force group = pi
-    create mask = 0660
-    directory mask = 0771
-    read only = no" >> /etc/samba/smb.conf
-    echo "done!"
-    echo "resetting smbd"
-    sudo systemctl restart smbd
-    sleep 5
-    echo "done"
-}
-
-
-sambaSetup () {
-    echo "Setting up samba"
-    cd /media
-    sudo mkdir USBDrive
-    sudo umount /dev/sda1
-    echo "mounting /dev/sda1 /media/USBDrive"
-    sudo mount /dev/sda1 /media/USBDrive
-    echo "Downloading samba..."
-    sudo apt-get install samba samba-common-bin
-    echo "creating backup for smb.conf"
-    cp /etc/samba/smb.conf /etc/samba/smb.conf.bak  #Make a backup of the config file
-    echo "Please enter samba password"
-    read pass
-    sudo smbpasswd -a $pass
-    echo "Please run setup.sh {sambaConfig} to update the config"
+    which micro > /dev/null
+    if [ $? -eq 0 ]; then
+        tput setaf 1; echo "micro already installed!"
+    else
+        echo "Downloading micro..."
+        curl https://getmic.ro | bash
+        tput setaf 3; echo "Changing permissions for micro"
+        sudo chown root:root micro
+        tput setaf 2; echo "Micro installation complete!"
+        sudo mv micro /usr/local/bin
+        tput setaf 3; echo "Moving micro to /usr/local/bin"
+        which micro
+    fi
 }
 
 case "$1" in
     screen)
         screenSetup
-        exit 1
         ;;
     micro)
         microSetup
-        exit 1
         ;;
     samba)
-        sambaSetup
-        exit 1
+        /usr/bin/bash smb.sh
         ;;
-    
-    sambaConfig)
-        smbConfWrite
-        exit 1
-        ;;
-    
     all)
         screenSetup
         microSetup
-        exit 1
+        /usr/bin/bash smb.sh
         ;;
     *)
-        echo "Usage: setup.sh {screen|micro|samba|all}"
+        tput setaf 5; echo "Usage: setup.sh {screen|micro|samba|all}"
         exit 1
         ;;
 esac
---
+++ smb.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/bash
+
+smbConfWrite () {
+    data ="
+[usb]
+    comment = Media Share
+    path = /media/USBDrive
+    writeable = yes
+    force group = pi
+    create mask = 0660
+    directory mask = 0771
+    read only = no
+
+[pi-share]
+    comment = Pi Share
+    path = /home/pi
+    writeable = yes
+    force group = pi
+    create mask = 0660
+    directory mask = 0771
+    read only = no"
+    if grep -q $data /etc/samba/smb.conf; then
+        tput setaf 1; echo "Data has already been written!"
+        tput setaf 2; echo "resetting smbd"
+        sudo systemctl restart smbd
+    else
+        echo $data >> /etc/samba/smb.conf
+        tput setaf 2; echo "done!"
+        tput setaf 2; echo "resetting smbd"
+        sudo systemctl restart smbd
+        tput setaf 2; echo "Config is ready."
+    fi
+    
+}
+
+
+sambaSetup () {
+    echo "Setting up samba"
+    which samba > /dev/null
+    if [ $? -eq 0 ]; then
+        tput setaf 1; echo "Samba already installed!"
+        exit 1
+    else
+        cd /media
+        sudo mkdir USBDrive
+        sudo umount /dev/sda1
+        tput setaf 3; echo "mounting /dev/sda1 /media/USBDrive"
+        sudo mount /dev/sda1 /media/USBDrive
+        tput setaf 3; echo "Downloading samba..."
+        sudo apt-get install samba samba-common-bin
+        tput setaf 3; echo "creating backup for smb.conf"
+        cp /etc/samba/smb.conf /etc/samba/smb.conf.bak  #Make a backup of the config file
+        tput setaf 3; echo "updating the smb.conf file"
+        smbConfWrite
+        tput setaf 5; echo "Please enter samba password"
+        read pass
+        sudo smbpasswd -a $pass
+        tput setaf 2; echo "Please run setup.sh {sambaConfig} to update the config"
+        exit 1
+    fi
+}
+
+sambaSetup
