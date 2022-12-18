1bae6bd9440014f46036d9ce701c9a06b2378187
DNAMcKnight <conner.cmk9040@gmail.com>
2022-12-18 23:01:38 +0600

    added fan controller for the raspberry pi

---
+++ raspiFan/setup.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+echo "moving fancontrol.sh to /etc/init.d"
+sudo mv fancontrol.sh /etc/init.d/
+echo "moving fancontrol.py to /usr/local/bin"
+sudo mv fancontrol.py /usr/local/bin/
+
+echo "adding execution permissions to fancontroler.py"
+sudo chmod +x /usr/local/bin/fancontrol.py
+echo "adding execution permissions to fancontroler.sh"
+sudo chmod +x /etc/init.d/fancontrol.sh
+
+sudo update-rc.d fancontrol.sh defaults
+echo "update-rc.d updated."
+echo "please reboot or run this command to start the script"
+echo "sudo /etc/init.d/fancontrol.sh start"
